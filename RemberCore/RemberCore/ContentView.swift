8278caccab64cbe2bd7ed2999801d19eaba8741b
aiexxash <ialexballa@gmail.com>
2024-08-02 15:57:30 +0300

    feat: Style first screen

--- RemberCore/RemberCore/ContentView.swift
+++ RemberCore/RemberCore/ContentView.swift
@@ -13,33 +13,70 @@ struct ContentView: View {
     @FetchRequest(sortDescriptors: [])
     var codes: FetchedResults<Code>
     @State private var codeNameText = ""
+    @State private var isErrorLabelVisible = false
     
     var body: some View {
-        VStack {
-            if codes.first != nil {
-                Text("not nil")
-            } else {
-                TextField("Enter your unique code", text: $codeNameText)
-                    .frame(width: UIScreen.main.bounds.width - 30)
-                    .padding(.horizontal, 15)
-                Button(action: {
-                    let code = Code(context: moc)
-                    code.id = UUID()
-                    code.name = codeNameText
+        NavigationStack {
+            VStack {
+                if codes.first != nil {
+                    Text("not nil")
+                } else {
+                    Spacer()
                     
-                    try? moc.save()
-                }) {
-                   Text("Add")
+                    TextField("Enter your unique code", text: $codeNameText)
+                        .frame(width: UIScreen.main.bounds.width - 30, height: 35)
+                        .font(.system(size: 16, weight: .regular))
+                        .padding(.horizontal, 15)
+                        .textFieldStyle(.roundedBorder)
+                    
+                    Button(action: {
+                        if codeNameText != "" {
+                            let code = Code(context: moc)
+                            code.id = UUID()
+                            code.name = codeNameText
+                            
+                            try? moc.save()
+                        } else {
+                            isErrorLabelVisible = true
+                        }
+                    }) {
+                        Text("Submit")
+                            .foregroundColor(.white)
+                    }
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 15)
+                        .frame(width: UIScreen.main.bounds.width - 30, height: 35)
+                        .font(.system(size: 16, weight: .semibold))
+                        .background(Color.accentColor)
                         .foregroundColor(.white)
+                        .cornerRadius(5)
+                    
+                    if isErrorLabelVisible == true {
+                        Text("errorLabel")
+                            .foregroundColor(Color.red)
+                            .font(.system(size: 14, weight: .regular))
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        print("New Group clicked")
+                    }) {
+                        Text("New Group")
+                    }
+                    .padding(.vertical, 15)
+                    .padding(.horizontal, 15)
+                    .frame(width: UIScreen.main.bounds.width - 30, height: 35)
+                    .font(.system(size: 16, weight: .semibold))
+                    .background(Color(UIColor.systemGray5))
+                    .foregroundColor(Color(UIColor.link))
+                    .cornerRadius(5)
                 }
-                .frame(width: UIScreen.main.bounds.width - 30, height: 35)
-                .background(Color.blue)
-                .cornerRadius(5)
-                .padding(.vertical, 10)
-                .padding(.horizontal, 15)
             }
+            .padding()
+            .navigationTitle("Rember")
+            .navigationBarTitleDisplayMode(.large)
         }
-        .padding()
     }
 }
 
