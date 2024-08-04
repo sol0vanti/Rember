b25a1a3591226b6b5e35834c128e2868b3ea39dc
aiexxash <ialexballa@gmail.com>
2024-08-04 09:03:39 +0300

    feat: Delete CoreData, use only Firebase

--- RemberCore/RemberCore/ContentView.swift
+++ RemberCore/RemberCore/ContentView.swift
@@ -19,6 +19,9 @@ struct ContentView: View {
     @State private var navigateToDetail = false
     @State private var errorText: String? = nil
     @State private var passwordText = ""
+    @State private var selectedName: String = ""
+    @State private var isSuccessful = false
+    @State private var documentExists: Bool = false
     
     init() {
         FirebaseApp.configure()
@@ -27,118 +30,118 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                if codes.first == nil {
-                    // MARK: Navigate to main view, placeholder below
-                    Text("nil")
-                } else {
-                    HStack {
-                        Text("Create your first folder")
-                            .font(.system(size: 18, weight: .semibold))
-                            .multilineTextAlignment(.leading)
-                            .foregroundColor(Color(UIColor.lightGray))
-                            .padding(.horizontal, 15)
-                        Spacer()
-                    }
-                    
-                    Spacer()
-                    
-                    TextField("Enter your unique code", text: $codeNameText)
-                        .frame(width: UIScreen.main.bounds.width - 30, height: 35)
-                        .font(.system(size: 16, weight: .regular))
+                HStack {
+                    Text("Create your first folder")
+                        .font(.system(size: 18, weight: .semibold))
+                        .multilineTextAlignment(.leading)
+                        .foregroundColor(Color(UIColor.lightGray))
                         .padding(.horizontal, 15)
-                        .textFieldStyle(.roundedBorder)
-                    
-                    TextField("Enter your password", text: $passwordText)
-                        .frame(width: UIScreen.main.bounds.width - 30, height: 35)
-                        .font(.system(size: 16, weight: .regular))
-                        .padding(.horizontal, 15)
-                        .textFieldStyle(.roundedBorder)
-                    
-                    Button(action: {
-                        if codeNameText != "" {
-                            let error: String? = self.checkFolderName()
-                            if error != nil {
-                                showAlert = true
-                            } else {
-                                let folderName = "folder_\(codeNameText)"
-                                
-                                let storage = Storage.storage()
-                                let storageRef = storage.reference().child(folderName)
-                                
-                                let emptyData = Data()
-                                let fileRef = storageRef.child("placeholder.txt")
-                                
-                                fileRef.putData(emptyData, metadata: nil) { metadata, error in
-                                    if let error = error {
-                                        print("Error creating folder: \(error.localizedDescription)")
-                                    } else {
-                                        let db = Firestore.firestore()
-                                        
-                                        db.collection("codes").document(folderName).setData(["password": passwordText]) { error in
-                                            if let error = error {
-                                                errorText = "Failed to save to Firestore."
-                                                print(error.localizedDescription)
-                                            } else {
-                                                let code = Code(context: moc)
-                                                code.id = UUID()
-                                                code.name = codeNameText
-                                                
-                                                do {
-                                                    try moc.save()
-                                                } catch {
-                                                    errorText = "Failed to save code.name to Core Data."
-                                                    print(error.localizedDescription)
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        } else {
-                            errorText = "Please, fill the text field and try again."
-                        }
-                    }) {
-                        Text("Create folder")
-                            .foregroundColor(.white)
-                    }
-                        .padding(.vertical, 10)
-                        .padding(.horizontal, 15)
-                        .frame(width: UIScreen.main.bounds.width - 30, height: 35)
-                        .font(.system(size: 16, weight: .semibold))
-                        .background(Color.accentColor)
-                        .foregroundColor(.white)
-                        .cornerRadius(5)
-                        .alert(isPresented: $showAlert) {
-                            Alert(title: Text("Failed to create new folder"), message: Text("It seems that this folder already exists."),
-                                  primaryButton: .destructive(Text("Log In")) {
-                                navigateToDetail = true
-                            },
-                            secondaryButton: .cancel())
-                        }
-                    
-                    NavigationLink(
-                        destination: DetailView(name: $codeNameText),
-                        isActive: $navigateToDetail,
-                        label: { EmptyView() }
-                    )
-                    
-                    if errorText != nil {
-                        Text(errorText ?? "An internal error was reported.")
-                            .foregroundColor(Color.red)
-                            .font(.system(size: 14, weight: .regular))
-                            .padding(.vertical, 10)
-                    }
-                    
                     Spacer()
                 }
+                
+                Spacer()
+                
+                TextField("Enter your unique code", text: $codeNameText)
+                    .frame(width: UIScreen.main.bounds.width - 30, height: 35)
+                    .font(.system(size: 16, weight: .regular))
+                    .padding(.horizontal, 15)
+                    .textFieldStyle(.roundedBorder)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                
+                TextField("Enter your password", text: $passwordText)
+                    .frame(width: UIScreen.main.bounds.width - 30, height: 35)
+                    .font(.system(size: 16, weight: .regular))
+                    .padding(.horizontal, 15)
+                    .textFieldStyle(.roundedBorder)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                
+                Button(action: {
+                    let folderName = "folder_\(codeNameText)"
+                    
+                    if codeNameText != "" {
+                        checkDocumentExists(name: folderName) { message in
+                            if message != nil {
+                                showAlert = true
+                            } else {
+                                if passwordText != "" {
+                                    let db = Firestore.firestore()
+                                    
+                                    db.collection("codes").document(folderName).setData(["password": passwordText]) { error in
+                                        if let error = error {
+                                            errorText = "Failed to save to Firestore."
+                                            print(error.localizedDescription)
+                                        } else {
+                                            isSuccessful = true
+                                        }
+                                    }
+                                } else {
+                                    errorText = "Please, fill the password text field and try again."
+                                }
+                            }
+                        }
+                    } else {
+                        errorText = "Please, fill the title text field and try again."
+                    }
+                }) {
+                    Text("Create folder")
+                        .foregroundColor(.white)
+                }
+                .padding(.vertical, 10)
+                .padding(.horizontal, 15)
+                .frame(width: UIScreen.main.bounds.width - 30, height: 35)
+                .font(.system(size: 16, weight: .semibold))
+                .background(Color.accentColor)
+                .foregroundColor(.white)
+                .cornerRadius(5)
+                .alert(isPresented: $showAlert) {
+                    Alert(title: Text("Failed to create new folder"), message: Text("It seems that this folder already exists."),
+                          primaryButton: .destructive(Text("Log In")) {
+                        navigateToDetail = true
+                    },
+                          secondaryButton: .cancel())
+                }
+                
+                NavigationLink(
+                    destination: DetailView(name: $codeNameText),
+                    isActive: $navigateToDetail,
+                    label: { EmptyView() }
+                )
+                
+                if errorText != nil {
+                    Text(errorText ?? "An internal error was reported.")
+                        .foregroundColor(Color.red)
+                        .font(.system(size: 14, weight: .regular))
+                        .padding(.vertical, 10)
+                }
+                
+                if isSuccessful {
+                    Text("You created a new folder!")
+                        .foregroundColor(.green)
+                        .font(.system(size: 14, weight: .regular))
+                        .padding(.vertical, 10)
+                }
+                
+                Spacer()
             }
             .navigationTitle("Welcome to Rember")
             .navigationBarTitleDisplayMode(.large)
         }
-        .onAppear {
-            self.fetchFolderNames()
+    }
+    
+    func checkDocumentExists(name: String, completion: @escaping (String?) -> Void) {
+        let db = Firestore.firestore()
+        
+        db.collection("codes").document(name).getDocument { (document, error) in
+            if let document = document, document.exists {
+                completion("The code is already existing")
+            } else {
+                completion(nil)
+            }
         }
     }
+}
     
 struct DetailView: View {
     @Binding var name: String
@@ -167,6 +170,9 @@ struct DetailView: View {
                     .font(.system(size: 16, weight: .regular))
                     .padding(.horizontal, 15)
                     .textFieldStyle(.roundedBorder)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                
                 Button(action: checkPassword) {
                     Text("Check Password")
                 }
@@ -219,30 +225,6 @@ struct DetailView: View {
 }
     
     // MARK: Functionality of app
-    
-    func fetchFolderNames() {
-            let storage = Storage.storage()
-            let storageRef = storage.reference()
-            
-            storageRef.listAll { (result, error) in
-                if let error = error {
-                    print("Error listing storage contents: \(error)")
-                    return
-                }
-                self.folderNames = result!.prefixes.map { $0.name }
-            }
-        }
-
-        func checkFolderName() -> String? {
-            let textToCheck = "folder_\(codeNameText)"
-            print(folderNames)
-            if folderNames.contains(textToCheck) {
-                return "Folder '\(textToCheck)' exists in Firebase Storage."
-            } else {
-                return nil
-            }
-        }
-}
 
 #Preview {
     ContentView()
