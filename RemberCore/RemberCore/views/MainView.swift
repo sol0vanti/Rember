d8dcce47c3e67181c7f0188cafafe3d935505766
aiexxash <ialexballa@gmail.com>
2024-08-05 14:56:46 +0300

    func: Add button functionality

--- RemberCore/RemberCore/views/MainView.swift
+++ RemberCore/RemberCore/views/MainView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct MainView: View {
     @Binding var folderCode: String
-    @State var showCreateAlert = false
-    var subFolders: [String] = ["Example folder 1", "Folder 2"]
+    @State private var showCreateAlert = false
+    @State private var alertTextFieldText = ""
+    @State private var subFolders: [String] = ["Example folder 1", "Folder 2"]
     
     var body: some View {
         NavigationStack {
@@ -24,12 +25,6 @@ struct MainView: View {
             .navigationBarBackButtonHidden(true)
             .navigationTitle(folderCode)
             .navigationBarTitleDisplayMode(.large)
-            .alert(isPresented: $showCreateAlert) {
-                Alert(title: Text("New gallery"), message: Text("In order to save your images you should create new subfolder"),
-                      primaryButton: .default(Text("Create")) {
-                    print("Create pressed")
-                }, secondaryButton: .cancel())
-            }
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     Button(action: {
@@ -37,10 +32,32 @@ struct MainView: View {
                     }) {
                         Image(systemName: "plus")
                             .imageScale(.large)
+                    }.alert(
+                        Text("New gallery"),
+                        isPresented: $showCreateAlert
+                    ) {
+                        Button("Cancel", role: .cancel) {}
+                        Button("Create") {
+                            if !alertTextFieldText.isEmpty {
+                                subFolders.append(alertTextFieldText)
+                                alertTextFieldText = ""
+                            }
+                        }
+
+                        TextField("Title", text: $alertTextFieldText)
+                            .textContentType(.creditCardNumber)
+                    } message: {
+                       Text("Please create new subfolder")
                     }
-                    
                 }
             }
         }
     }
 }
+
+/* MARK: For future use
+ MARK: Cannot use mutating member on immutable value: 'self' is immutable
+ Fix:
+ If it is a property of struct - put @State before
+ Else if it is function, put the word mutating before
+ */
