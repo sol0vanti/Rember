e635ce3521396157ab99d618c5114777cf8ad28e
aiexxash <ialexballa@gmail.com>
2024-08-05 13:13:44 +0300

    feat: Create structure & Start MainView coding

---
+++ RemberCore/RemberCore/views/MainView.swift
@@ -0,0 +1,16 @@
+//
+//  MainView.swift
+//  RemberCore
+//
+//  Created by Alex Balla on 05.08.2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+    }
+}
